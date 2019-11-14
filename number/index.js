ccfc0baf922ce4432c9e06425c74a1930e843256
Minionb <43756995+Minionb@users.noreply.github.com>
2019-11-14 14:10:29 +0800

    The first commit

---
+++ number/index.js
@@ -0,0 +1,12 @@
+var result = "";
+for (var i=1; i<=20; i++){
+    if (i%2 == 1){
+        result += i + " is odd number.\n" ;
+    }
+    else{
+        result += i + " is even number.\n" ;
+
+    }
+
+}
+console.log(result);
